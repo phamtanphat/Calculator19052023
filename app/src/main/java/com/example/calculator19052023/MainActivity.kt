a7a4ad9d7cd86da15cbf2cd28eea7b559beeaee6
phamtanphat <phatdroid94@gmail.com>
2023-06-26 18:53:42 +0700

    event click

--- app/src/main/java/com/example/calculator19052023/MainActivity.kt
+++ app/src/main/java/com/example/calculator19052023/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.calculator19052023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edtNumber1: EditText
+    private lateinit var edtNumber2: EditText
+    private lateinit var tvResult: TextView
+    private lateinit var btnPlus: Button
+    private lateinit var btnMinus: Button
+    private lateinit var btnMultiplication: Button
+    private lateinit var btnDivision: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Ánh xạ
+        edtNumber1 = findViewById(R.id.edit_text_number_1)
+        edtNumber2 = findViewById(R.id.edit_text_number_2)
+        tvResult = findViewById(R.id.text_view_result)
+        btnPlus = findViewById(R.id.button_plus)
+        btnMinus = findViewById(R.id.button_minus)
+        btnMultiplication = findViewById(R.id.button_multiplication)
+        btnDivision = findViewById(R.id.button_division)
+
+        btnPlus.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+
+            }
+        })
     }
 }
